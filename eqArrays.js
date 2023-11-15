234395f82a609a076bba84a9399dfa5860c728de
Jonathan Lao <jon.lao@gmail.com>
2023-11-14 22:51:56 -0600

    updated eqArray.js based on AI suggestions

--- eqArrays.js
+++ eqArrays.js
@@ -24,3 +24,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], []), true);
