16630dbf9c99d0d1537ff93314eee6a5836136c1
Jonathan Lao <jon.lao@gmail.com>
2023-11-16 22:43:41 -0600

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (array, callback) => {
+  for (const key of Object.keys(array)) {
+    if (callback(array[key])) {
+      return key;
+    }
+  }
+};
+
+
+const key = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+const key2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3); // => "Akaleri"
+
+assertEqual(key, "noma");
+assertEqual(key2, "noma"); // should fail
