5224b1d6c3a3d15f93ccb099d4d05df7323382e7
Jonathan Lao <jon.lao@gmail.com>
2023-11-16 21:10:52 -0600

    added map.js

---
+++ map.js
@@ -0,0 +1,29 @@
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`❤️  Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = (array1, array2) =>
+  array1.length === array2.length &&
+    array1.every((element, index) => element === array2[index]);
+
+// map.js
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
