774e88e74b111b77cb36baff3fb00ab0d22fdf02
Jonathan Lao <jon.lao@gmail.com>
2023-11-20 17:57:34 -0600

    refactored tail.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -6,5 +6,5 @@ const tail = require('../tail');
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
+tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
