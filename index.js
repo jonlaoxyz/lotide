0e8fc72c3a35a22adf2ca1489a06ba029c496d88
Jonathan Lao <jon.lao@gmail.com>
2023-11-20 22:21:36 -0600

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const tail   = require('./tail');
 const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
-const assertArraysEqual = require('./assertArraysEqual')
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   head:   head,
@@ -12,5 +22,16 @@ module.exports = {
   middle: middle,
   eqArrays: eqArrays,
   assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
+  without: without
+
 };
