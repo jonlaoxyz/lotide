4e76a74b07bfeb03f23f6a43e2514e69b01de52e
Jonathan Lao <jon.lao@gmail.com>
2023-11-20 20:58:47 -0600

    Refactored head with Mocha/Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,21 +1,14 @@
-const eqArrays = require('../eqArrays');
+
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 
 
 //  Test Cases
-console.log(middle([]));
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
-console.log(assertArraysEqual(middle([1]), [])); // => []
-console.log(assertArraysEqual(middle([1, 2, 3]), [2])); // => [2]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])); // => [3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])); // => [2, 3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])); // => [3, 4]
-console.log(assertArraysEqual(middle([1, 2, 3]), [3])); // => [2]
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+assertArraysEqual(middle([1, 2, 3]), [3]); // => [2]
